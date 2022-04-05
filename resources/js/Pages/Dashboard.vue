8dea973239953e9076f006fe257ca3157a7068cf
fredrick rogath <fredlee560@gmail.com>
2022-04-05 19:34:44 +0300

    marian

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -24,6 +24,23 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import Welcome from "@/Jetstream/Welcome.vue";
 import PostCard from "@/Jetstream/MyComponents/PostCard.vue";
 
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 250,
+
+  // The color of the progress bar.
+  color: '#29d',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: true,
+})
+
 export default defineComponent({
   components: {
     AppLayout,
